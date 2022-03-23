201270c967f80ddac6e4e0f921cff7df9e27b1fb
khoadangtruong <khoadangtruong99@gmail.com>
2022-03-23 17:26:15 +0700

    test commit

--- myuser/signals.py
+++ myuser/signals.py
@@ -1,4 +1,5 @@
-from django.dispatch import receiver
+from django.db import transaction
+from django.dispatch import receiver, Signal
 from django.db.models.signals import post_save
 from django.contrib.auth.models import User
 from .models import Profile
@@ -9,7 +10,8 @@ def create_profile(sender, instance, created, **kwargs):
     if created:
         Profile.objects.create(user = instance)
         print(f"{instance.username}'s profile is created! Sending confirm email")
-        send_confirmation_mail_task.delay(instance.email)
+        transaction.on_commit(lambda: send_confirmation_mail_task.delay(instance.email))
     else:
         instance.profile.save()
         print(f"{instance.username}'s profile is saved!")
+
